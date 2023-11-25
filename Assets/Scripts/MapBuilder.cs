35877930453b7c6874f11bfdd4dc5af6f1338a94
Igor Shnayder <shnayder.ig@gmail.com>
2023-11-25 19:18:15 +0100

    Update

--- Assets/Scripts/MapBuilder.cs
+++ Assets/Scripts/MapBuilder.cs
@@ -7,127 +7,52 @@ public class MapBuilder : MonoBehaviour
     /// В качестве параметра передается префаб тайла, изображенный на кнопке.
     /// Вы можете использовать префаб tilePrefab внутри данного метода.
     /// </summary>
-    
+    [SerializeField] private Map _map;
     [SerializeField] private Grid _grid;
-    [SerializeField] private Material _highlightingMaterialPutTale;
-    [SerializeField] private Material _highlightingMaterialNotPutTale;
     
-    private GameObject _tilePrefab;
-    private Camera _mainCamera;
-    private Material[] _basicMaterials;
-    private bool _isItGameField;
-    private Collider _hitCollider;
-    private Renderer[] _spriteElements;
+    private Tile _currentTile;
+    private Camera _camera;
     
     private void Awake()
     {
-        _mainCamera = Camera.main;
+        _camera = Camera.main;
+    }
+    
+    public void StartPlacingTile(GameObject tilePrefab)
+    {
+        var tileObject = Instantiate(tilePrefab, _map.transform);
+        _currentTile = tileObject.GetComponent<Tile>();
     }
     
     private void Update()
     {
-        if (_tilePrefab == null) return;
+        if (_currentTile == null) return;
         PutTileOnMap();
-    }
-
-    public void StartPlacingTile(GameObject tilePrefab)
-    {
-        if (_tilePrefab != null) return;
-            
-        _tilePrefab = Instantiate(tilePrefab);
-        SetLayerRecursivelyToChildren(_tilePrefab.transform, 2);
-        TakeTileRenders();
-        SaveTileBasicMaterials();
     }
     
     private void PutTileOnMap()
     {
         var mousePosition = Input.mousePosition;
-        var mousePositionRay = _mainCamera.ScreenPointToRay(mousePosition);
+        var ray = _camera.ScreenPointToRay(mousePosition);
         
-        if (Physics.Raycast(mousePositionRay, out var hitInfo))
+        if (Physics.Raycast(ray, out var hitInfo))
         {
             var worldPosition = hitInfo.point;
-            _hitCollider = hitInfo.collider;
-            
             var cellPosition = _grid.WorldToCell(worldPosition);
             var cellCenterWorld = _grid.GetCellCenterWorld(cellPosition);
         
-            _tilePrefab.transform.position = cellCenterWorld;
-        
-            TakeTileRenders();
-            DefiningObjectUnderTile();
-            Highlighting();
+            _currentTile.transform.position = cellCenterWorld;
 
-            if (!_isItGameField) return;
+            var isAvailable = _map.IsCellAvailable(cellPosition);
+            _currentTile.Highlighting(isAvailable);
+            
+            if (!isAvailable) return;
 
             if (!Input.GetMouseButtonDown(0)) return;
             
-            RestoreTileBasicMaterials();
-        
-            _tilePrefab.transform.position = cellCenterWorld;
-            SetLayerRecursivelyToChildren(_tilePrefab.transform, 0);
-            _tilePrefab = null;
-        }
-    }
-
-    private void Highlighting()
-    {
-        if (_isItGameField)
-        {
-            foreach (var renderer in _spriteElements)
-            {
-                renderer.material = _highlightingMaterialPutTale;
-            }
-
-            return;
-        }
-
-        foreach (var renderer in _spriteElements)
-        {
-            renderer.material = _highlightingMaterialNotPutTale;
-        }
-    }
-
-    private void TakeTileRenders()
-    {
-        _spriteElements = _tilePrefab.GetComponentsInChildren<Renderer>();
-    }
-
-    private void SaveTileBasicMaterials()
-    {
-        _basicMaterials = new Material[_spriteElements.Length];
-        
-        for (var i = 0; i < _basicMaterials.Length; i++)
-        {
-            _basicMaterials[i] = _spriteElements[i].material;
-        }
-    }
-
-    private void RestoreTileBasicMaterials()
-    {
-        for (var i = 0; i < _spriteElements.Length; i++)
-        {
-            _spriteElements[i].material = _basicMaterials[i];
-        }
-    }
-
-    private void DefiningObjectUnderTile()
-    {
-        _isItGameField = false;
-        
-        if (!_hitCollider.CompareTag("GameField")) return;
-        
-        _isItGameField = true;
-    }
-    
-    private void SetLayerRecursivelyToChildren(Transform currentTransform, int layer)
-    {
-        currentTransform.gameObject.layer = layer;
-        
-        foreach (Transform child in currentTransform)
-        {
-            SetLayerRecursivelyToChildren(child, layer);
+            _map.SetTile(cellPosition, _currentTile);
+            _currentTile.ResetMaterialsColor();
+            _currentTile = null;
         }
     }
 }
